d8b52e6c4d54bdf471174403842012b3c2f32c7c
Alberto Gualis <agualis@gmail.com>
2017-04-19 08:56:52 +0200

    Show Sessions component as home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Sessions from '@/pages/Sessions';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'Sessions',
+      component: Sessions,
     },
   ],
 });
