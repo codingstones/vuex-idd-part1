ccf7a24765dc205b95381b810c426de1d0186a8a
Alberto Gualis <agualis@gmail.com>
2017-04-20 10:32:09 +0200

    Add first vuex store with sessions

---
+++ src/vuex/store.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import { buildRetrieveSessions } from './actions';
+import { fetchSessions, fetchSessionsError, fetchSessionsSuccess } from './mutations';
+
+Vue.config.debug = true;
+Vue.use(Vuex);
+
+const initialState = {
+  sessions: [],
+  loading: false,
+  error: '',
+};
+
+export const store = new Vuex.Store({
+  state: initialState,
+  mutations: {
+    FETCH_SESSIONS: fetchSessions,
+    FETCH_SESSIONS_SUCCESS: fetchSessionsSuccess,
+    FETCH_SESSIONS_ERROR: fetchSessionsError,
+  },
+  actions: {
+    retrieveSessions: buildRetrieveSessions(),
+  },
+  getters: {
+    sessions: state => state.sessions,
+    isLoading: state => state.loading,
+    error: state => state.error,
+  },
+});
+
